61b69ef14ba898d6d44fe8973cff09b2e1c8cd3d
Tony-sys <emmanuel_general@hotmail.com>
2021-02-07 22:03:00 -0600

    ScrollReveal - Custom Animations | End Proyect

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,74 @@
 /*-------------------------------------------- 
 [Table of contents]
 
-    1. Nav - Effect
+    A. Nav - Side Display
+
+    B. Animations
 ----------------------------------------------*/
-/*=============================================
-=          START SECTION | Nav Effect         =
-Desc: Select element function  
-=============================================*/
+
+/*********************************************
+A. Nav - Side Display
+Description: Select element function  
+*********************************************/
+
 const selectElement = function (element) { 
+
     return document.querySelector(element);
+
 };
 
 let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
 
 menuToggler.addEventListener('click', function () {
+
     body.classList.toggle('open');
+
+});
+
+/************** END OF | Nav - Side Display  **************/
+
+/*********************************************
+B. Section
+*********************************************/
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.animate-left', {
+
+    origin: 'left',
+    duration: '1000', //Duration of animation
+    distance: '25rem', //Traveling From
+    delay: 300,//How long it should it take
+
 });
 
 
+sr.reveal('.animate-right', {
+
+    origin: 'right',
+    duration: '1000', //Duration of animation
+    distance: '25rem', //Traveling From
+    delay: 600,//How long it should it take
+
+});
+
+sr.reveal('.animate-top', {
+
+    origin: 'top',
+    duration: '1000', //Duration of animation
+    distance: '25rem', //Traveling From
+    delay: 600,//How long it should it take
+
+});
+
+sr.reveal('.animate-bottom', {
+
+    origin: 'bottom',
+    duration: '1000', //Duration of animation
+    distance: '25rem', //Traveling From
+    delay: 600,//How long it should it take
+
+});
+
+/************** END OF | section  **************/
 
